93a937ec0098d16011b0c0375465a068237e15f6
Clarky <clarky117@outlook.com>
2022-10-23 23:27:33 +0800

    deployment attempt

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+require("dotenv").config({ path: ".config.env" });
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/fishDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
